25fb659dbf387880de0a4bd31f964c773d88e61f
clarissacomendador <85914218+clarissacomendador@users.noreply.github.com>
2023-07-10 19:07:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+      $(document).ready(function(){
+        $('.btn').click(function(){
+          $('.items').toggleClass("show");
+          $('ul li').toggleClass("hide");
+        });
+      });
+   </script>
